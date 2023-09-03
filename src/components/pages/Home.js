aad0cf5afc7e16aab34e62e56603a2916c31c14b
Tiernan Green <tiernanjamesgreen@gmail.com>
2023-09-03 20:34:37 +0100

    initial files set up

---
+++ src/components/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div id="home">
+      <div>
+        <h1>Welcome to my Portfolio</h1>
+        <p>
+          Hi there! I've created this portfolio using REACT which is quite exciting! I hope you like what you see, feel free to visit the contact page for more information on
+         how to get in contact with me! Enjoy!
+       </p>
+        </div>
+        <div>
+          <img src="https://cdn3.vectorstock.com/i/1000x1000/16/87/have-fun-hand-lettering-vector-5081687.jpg"></img>
+        </div>
+    </div>
+  );
+}
