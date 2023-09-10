7b77a1dbf4134e29969992330e1e09ec449843f8
Tiernan Green <tiernanjamesgreen@gmail.com>
2023-09-10 21:33:15 +0100

    finished the portfolio for now

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,12 +5,9 @@ export default function About() {
     <div>
       <h1>About Page</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+       My name is Tiernan Green. I've been learning how to code for the last six months, starting with the basics of CSS, Javascript and HTML and finally recently moving on
+       to full-stack web applications. It's been a difficult journey, however I am now more confident in my abilities and a lot more knowledgeable.
+       Anything I do not know presently, I am certain with research and practice I will be able to puck up very quickly! 
       </p>
     </div>
   );
---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Portfolio() {
+  return (
+    <div>
+    <div class="portfolio1">
+      <h1>Portfolio Page</h1>
+      <h2><a href="https://github.com/Tiernan-97/Work-Day-Scheduler" target="_blank" >Work-Day-Scheduler</a></h2>
+      <h2><a href="https://github.com/Tiernan-97/README_Generator" target="_blank">README-Generator</a></h2>
+      <h2><a href="https://github.com/Tiernan-97/Password_Generator" target="_blank">Password-Generator</a></h2>
+      <h2><a href="https://github.com/Tiernan-97/SVG-logo-Generator" target="_blank">SVG-Logo-Generator</a></h2>
+      <h2><a href="https://github.com/Tiernan-97/PWA_Text_Editor" target="_blank">PWA-Text-Editor</a></h2>
+      <h2><a href="https://github.com/Tiernan-97/Weather-Forecast" target="_blank">Weather-Forecast</a></h2>
+      <h2><a href="https://github.com/Tiernan-97/Social_Network_API" target="_blank">Social-Network-API</a></h2>
+    </div>
+    </div>
+  );
+}
