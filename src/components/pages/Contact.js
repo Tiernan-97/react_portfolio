dcc6a13f046db2c9a657201705dd072006ba5bcf
Tiernan Green <tiernanjamesgreen@gmail.com>
2023-09-10 22:00:30 +0100

    updated README

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,8 +5,11 @@ export default function Contact() {
     <div>
       <h1>Contact Page</h1>
       <p>
-        If you would to get in contact with me, you can email me at: </p>
-        <a href="mailto:tiernanjamesgreen@gmail.com">tiernanjamesgreen@gmail.com</a>
+        Here's some ways to see more of my work or get in contact with me:
+      </p>
+      <a href="https://github.com/Tiernan-97" target="_blank">My GitHub</a><br></br>
+      <a href="https://www.linkedin.com/in/tiernangreen/" target="_blank">My LinkedIn</a><br></br>
+      <a href="https://twitter.com/tbreezyy97" target="_blank">My Twitter</a>
     </div>
   );
 }
